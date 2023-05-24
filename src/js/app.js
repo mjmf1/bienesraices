13f117d56be0aec11188109d7a364bbf12f3610e
mjmf1 <marcomora61@gmail.com>
2023-05-24 09:38:06 -0500

    Leer las preferencias del sistema

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,26 @@ document.addEventListener('DOMContentLoaded', function () {
     darkMode();
 });
 
-function darkMode(){
+function darkMode(){ // darkmode
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+   // console.log(prefiereDarkMode);
+
+   if(prefiereDarkMode.matches){
+    document.body.classList.add('dark-mode');
+   }else{
+    document.body.classList.remove('dark-mode');
+   }
+   prefiereDarkMode.addEventListener('change', function(){
+
+    if(prefiereDarkMode.matches){
+        document.body.classList.add('dark-mode');
+       }else{
+        document.body.classList.remove('dark-mode');
+       }
+   });
+
 
     const botonDarkMode = document.querySelector('.dark-mode-boton');
 
