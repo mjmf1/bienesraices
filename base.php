e6b8c0efff9c9546b3bd326643fd68909814d393
mjmf1 <marcomora61@gmail.com>
2023-05-26 10:25:19 -0500

    Creando un Header (Windows)

---
+++ base.php
@@ -0,0 +1,24 @@
+<?php 
+ include './includes/templates/header.php';
+ ?>
+
+    <main class="contenedor seccion">
+        <h1>base</h1>
+    </main>
+
+    <footer class="footer seccion">
+        <div class="contenedor contenedor-footer">
+            <nav class="navegacion">
+                <a href="nosotros.html">Nosotros</a>
+                <a href="anuncios.html">Anuncios</a>
+                <a href="blog.html">Blog</a>
+                <a href="contacto.html">contacto</a>
+            </nav>
+        </div>
+        <p class="copyright">Todos los Derechos Reservados 2023 &copy;</p>
+    </footer>
+    
+    <script src="src/js/modernizr.js"></script>
+    <script src="src/js/app.js"></script>
+</body>
+</html>
