bd875ae49d692f4a3aa44f681b34d6c982b25304
mjmf1 <marcomora61@gmail.com>
2023-06-23 06:30:06 -0500

    Creando una tabla para listar las propiedades de la base de datos

--- admin/index.php
+++ admin/index.php
@@ -17,6 +17,32 @@ $resultado = $_GET['resultado'] ?? null;
 
 
         <a href="/bienesraices/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propieda</a>
+
+        <table class="propiedades">
+            <thead>
+
+            <tr>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+                
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la Playa</td>
+                    <td><img src="/bienesraices/imagenes/1be769320b0cb66e1fecdad94e3f950d.jpg" class="imagen-tabla" alt="imagenes"></td>
+                    <td>$1200000</td>
+                    <td>
+                    <a href="#" class="boton-rojo-block">Eliminar</a>
+                    <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+        </table>
+
     </main>
 
     <?php 
