03b78ec691d253f12d36b0d8facf92191615f6f5
mjmf1 <marcomora61@gmail.com>
2023-08-16 10:50:55 -0500

    Hashear Passwords

--- usuario.php
+++ usuario.php
@@ -7,12 +7,15 @@ $conn = conectarDB();
 // Email y contraseña
 $email = "correo@correo.com";
 $password = "123456";
+$passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+var_dump($passwordHash);
 
 //Query para crear el usuario
 
-$query = "INSERT INTO usuarios (email, password) VALUES ('${email}', '${password}') ";
+$query = "INSERT INTO usuarios (email, password) VALUES ('${email}', '${passwordHash}') ";
 
-echo $query;
+// echo $query;
 
 //Agregarlo a la base de datos
 mysqli_query($conn,$query);
